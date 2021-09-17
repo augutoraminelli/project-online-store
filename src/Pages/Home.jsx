ffc89bcb27dccafe022f2f864f869d66763c275e
Williamns <williamnstolb@gmail.com>
2021-09-17 17:36:55 -0300

    Requisito completo: 2 (agora sim, de novo)

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,7 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <input type="text" placeholder="Digite seu texto aqui."/>
+        <input type="text" placeholder="Digite seu texto aqui." />
         <h3 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h3>
