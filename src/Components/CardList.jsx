2c5ff2078bb981cf80912698bea4a15cbdb6ddb1
Williamns <williamnstolb@gmail.com>
2021-09-19 13:43:47 -0300

    Requisito: 5 - em progresso...

--- src/Components/CardList.jsx
+++ src/Components/CardList.jsx
@@ -1,18 +1,51 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 
 class CardList extends Component {
+  constructor() {
+    super();
+
+    this.productList = this.productList.bind(this);
+
+    this.state = {
+      title: '',
+      thumbnail: '',
+      price: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.productList();
+  }
+
+  // categoria genérica para testes
+  async productList() {
+    const { productSearch } = this.props;
+    const productList = await getProductsFromCategoryAndQuery('MLB430637', productSearch);
+    console.log(productList.results);
+    this.setState({
+
+    });
+  }
+
   render() {
+    const { productSearch } = this.props;
+    const { title, thumbnail, price} = this.state;
     return (
       <div>
-        
+        <section>
+          testando CardList
+          {' '}
+          {productSearch}
+        </section>
       </div>
     );
   }
 }
 
 CardList.propTypes = {
-
+  productSearch: PropTypes.string.isRequired,
 };
 
 export default CardList;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import CardList from '../Components/CardList';
 
 class Home extends Component {
+  constructor() {
+    super();
+
+    this.setSearch = this.setSearch.bind(this);
+
+    this.state = {
+      productSearch: '',
+    };
+  }
+
+  setSearch(event) {
+    const { value } = event.target;
+    this.setState({ productSearch: value });
+  }
+
   render() {
+    const { productSearch } = this.state;
     return (
       <div>
         <form method="get">
-          <input type="text" placeholder="Digite seu texto aqui. " />
+          <input
+            type="text"
+            placeholder="Digite seu texto aqui. "
+            data-testid="query-input"
+            value={ productSearch }
+            onChange={ this.setSearch }
+          />
           <h3 data-testid="home-initial-message">
             Digite algum termo de pesquisa ou escolha uma categoria.
           </h3>
@@ -14,6 +37,7 @@ class Home extends Component {
         <div>
           <Link to="/cart" data-testid="shopping-cart-button"><img alt="cart" src="https://img.icons8.com/external-flatart-icons-lineal-color-flatarticons/64/000000/external-cart-grocery-flatart-icons-lineal-color-flatarticons.png" /></Link>
         </div>
+        <CardList productSearch={ productSearch } />
       </div>
     );
   }
