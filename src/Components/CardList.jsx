ed8cd10b08bf1dfbfa8be24b61c94068c15d3ba0
Williamns <williamnstolb@gmail.com>
2021-09-19 11:46:01 -0300

    Requisito: 5 iniciado - pasta components criada com CardList

---
+++ src/Components/CardList.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class CardList extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+CardList.propTypes = {
+
+};
+
+export default CardList;
