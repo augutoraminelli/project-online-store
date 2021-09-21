5c0bd4cfeba96163851bb0ed401fa48ed62bf062
Gabriel <gabrielferreiradsl@hotmail.com>
2021-09-21 17:39:20 -0300

    requisito 8 finalizado, denovo

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -4,25 +4,24 @@ import PropTypes from 'prop-types';
 class Cart extends Component {
   render() {
     const { location: { state: { listOfCartProducts } } } = this.props;
+    console.log(listOfCartProducts);
     return (
       <section>
-        <div>
-          {(!listOfCartProducts) ? (
-            <h4 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h4>
-          )
-            : listOfCartProducts.map((product) => (
-              <section key={ product.id } data-testid="product">
-                <h4 data-testid="shopping-cart-product-name">
-                  { product.title }
-                </h4>
-                <img src={ product.thumbnail } alt={ product.title } />
-                <h5>{ `R$ ${product.price}` }</h5>
-                <button type="button">-</button>
-                <span data-testid="shopping-cart-product-quantity">1</span>
-                <button type="button">+</button>
-              </section>
-            ))}
-        </div>
+        {(listOfCartProducts.length === 0) ? (
+          <h4 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h4>
+        )
+          : listOfCartProducts.map((product) => (
+            <section key={ product.id } data-testid="product">
+              <h4 data-testid="shopping-cart-product-name">
+                { product.title }
+              </h4>
+              <img src={ product.thumbnail } alt={ product.title } />
+              <h5>{ `R$ ${product.price}` }</h5>
+              <button type="button">-</button>
+              <span data-testid="shopping-cart-product-quantity">1</span>
+              <button type="button">+</button>
+            </section>
+          ))}
       </section>
     );
   }
