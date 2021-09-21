fbcca7743247c8e59f5b09e755ef2e21ed8ad075
Gabriel <gabrielferreiradsl@hotmail.com>
2021-09-20 21:26:47 -0300

    requisito 7 em andamento

--- src/Pages/ProductDetails.jsx
+++ src/Pages/ProductDetails.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
 export default class ProductDetails extends Component {
-    render() {
-      console.log(this);
-        return (
-            <div>
-                <header>Teste</header>
-            </div>
-        )
-    }
+  render() {
+    console.log(this);
+    return (
+      <div>
+        <header>Teste</header>
+      </div>
+    );
+  }
 }
