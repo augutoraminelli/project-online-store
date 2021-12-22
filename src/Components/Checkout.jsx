9259857f0c3e6ca739a734d3415b43ece14611f3
Gabriel <gabrielferreiradsl@hotmail.com>
2021-12-22 20:52:48 -0300

    ajustes do componente checkout

--- src/Components/Checkout.jsx
+++ src/Components/Checkout.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { Redirect } from 'react-router';
 import Home from '../Pages/Home';
+import { CheckoutInputs } from './CheckoutInputs';
 
 class Checkout extends Component {
   constructor() {
@@ -27,8 +28,9 @@ class Checkout extends Component {
     const { redirect } = this.state;
     return (
       <section>
-        <Link to="/cart" data-testid="shopping-cart-button"><img alt="cart" src="https://img.icons8.com/external-becris-lineal-becris/64/000000/external-back-arrow-mintab-for-ios-becris-lineal-becris.png" /></Link>
+
         <div>
+          <Link to="/cart" data-testid="shopping-cart-button"><img alt="cart" src="https://img.icons8.com/external-becris-lineal-becris/64/000000/external-back-arrow-mintab-for-ios-becris-lineal-becris.png" /></Link>
           <h1 className="font-medium text-lg">Revise seus Produtos</h1>
           <h3
             className="font-sans
@@ -75,213 +77,8 @@ class Checkout extends Component {
            text-lg mt-10 mb-2 text-center"
           >
             Informações do comprador
-
           </h4>
-          <form className="form-checkout">
-            <div className="flex flex-wrap -mx-3 mb-6">
-              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                <label
-                  className="block uppercase
-                 tracking-wide text-gray-700 text-xs font-bold mb-2"
-                  htmlFor="grid-first-name"
-                >
-                  Nome completo
-                </label>
-                <input
-                  className="appearance-none
-                  block w-full bg-gray-200 text-gray-700 border
-                  border-red-500 rounded py-3 px-4 mb-3 leading-tight
-                  focus:outline-none focus:bg-white"
-                  id="grid-first-name"
-                  type="text"
-                  placeholder="Nome Completo"
-                />
-                <p className="text-red-500 text-xs italic">Please fill out this field.</p>
-              </div>
-              <div className="w-full md:w-1/2 px-3">
-                <label
-                  className="block
-                 uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                  htmlFor="grid-last-name"
-                >
-                  CPF
-                </label>
-                <input
-                  className="appearance-none
-                  block w-full
-                  bg-gray-200
-                  text-gray-700
-                  border
-                  border-gray-200
-                  rounded py-3
-                  px-4 leading-tight
-                  focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="grid-last-name"
-                  type="number"
-                  placeholder="CPF"
-                />
-              </div>
-            </div>
-            <div className="flex space-x-3 ">
-              <div className="flex flex-wrap -mx-3 mb-6">
-                <div className="w-full px-3">
-                  <label
-                    className="block
-                 uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                    htmlFor="grid-first-name"
-                  >
-                    Endereço
-                  </label>
-                  <input
-                    className="appearance-none
-                  block w-full bg-gray-200 text-gray-700
-                  border border-gray-200 rounded py-3 px-4 mb-3
-                  leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                    id="grid-email"
-                    type="text"
-                    placeholder="Endereço"
-                  />
-                </div>
-              </div>
-              <div className="flex flex-wrap -mx-3 mb-6">
-                <div className="w-full px-3">
-                  <label
-                    className="block
-                 uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                    htmlFor="grid-password"
-                  >
-                    Email
-                  </label>
-                  <input
-                    className="appearance-none
-                 block w-full bg-gray-200
-                  text-gray-700 border border-gray-200 rounded py-3
-                   px-4 mb-3 leading-tight focus:outline-none focus:bg-white
-                    focus:border-gray-500"
-                    id="grid-password"
-                    type="email"
-                    placeholder="Email"
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="flex flex-wrap -mx-3 mb-2">
-              <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                <label
-                  className="block uppercase tracking-wide
-                 text-gray-700 text-xs font-bold mb-2"
-                  htmlFor="grid-city"
-                >
-                  Cidade
-                </label>
-                <input
-                  className="appearance-none
-                 block w-full bg-gray-200 text-gray-700 border border-gray-200
-                  rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white
-                   focus:border-gray-500"
-                  id="grid-city"
-                  type="text"
-                />
-              </div>
-              <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                <label
-                  className="block uppercase tracking-wide
-                 text-gray-700 text-xs font-bold mb-2"
-                  htmlFor="grid-state"
-                >
-                  Estado
-                </label>
-                <div className="relative">
-                  <select
-                    className="block appearance-none
-                   w-full bg-gray-200 border
-                    border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded
-                     leading-tight focus:outline-none
-                     focus:bg-white focus:border-gray-500"
-                    id="grid-state"
-                  >
-                    <option>SP</option>
-                    <option>RJ</option>
-                    <option>MG</option>
-                  </select>
-                  <div
-                    className="pointer-events-none absolute
-                   inset-y-0 right-0 flex items-center px-2 text-gray-700"
-                  >
-                    <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
-                  </div>
-                </div>
-              </div>
-              <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                <label
-                  className="block uppercase tracking-wide
-                 text-gray-700 text-xs font-bold mb-2"
-                  htmlFor="grid-zip"
-                >
-                  CEP
-                </label>
-                <input
-                  className="appearance-none
-                 block w-full bg-gray-200
-                  text-gray-700 border border-gray-200
-                   rounded py-3 px-4
-                    leading-tight focus:outline-none
-                     focus:bg-white focus:border-gray-500"
-                  id="grid-zip"
-                  type="text"
-                />
-              </div>
-              <div className=" w-full flex justify-center mt-6">
-                <div>
-                  <label
-                    className="block uppercase
-                tracking-wide text-gray-700
-                 text-xs font-bold mb-2"
-                    htmlFor="grid-zip"
-                  >
-                    Telefone
-                  </label>
-                  <input
-                    className="appearance-none
-                 block w-full bg-gray-200 text-gray-700 border
-                  border-gray-200 rounded py-3 px-4
-                   leading-tight focus:outline-none
-                    focus:bg-white
-                     focus:border-gray-500"
-                    id="grid-zip"
-                    type="text"
-                    placeholder="Telefone"
-                  />
-                </div>
-              </div>
-            </div>
-          </form>
-          <div className="flex-col flex-wrap">
-            <h4
-              className="mt-10 font-sans text-lg
-             text-gray-800 text-center font-bold mb-5"
-            >
-              {' '}
-              Forma de pagamento
-
-            </h4>
-          </div>
-          <div className="flex-col mb-5 mt-10 payment-check space-x-2">
-            <div className="flex payment-check space-x-2">
-              <img alt="card" src="https://img.icons8.com/ios/50/000000/bank-card-back-side.png" />
-              <input type="radio" name="payment" id="Visa" />
-              <label htmlFor="Visa">Visa</label>
-              <input type="radio" name="payment" id="Mastercard" />
-              <label htmlFor="Mastercard">Mastercard</label>
-              <input type="radio" name="payment" id="Elo" />
-              <label htmlFor="Elo">Elo</label>
-            </div>
-            <div className="flex payment-check space-x-2">
-              <img alt="bank slip" src="https://img.icons8.com/ios/50/000000/barcode.png" />
-              <input type="radio" name="payment" id="Boleto" />
-              <label htmlFor="Boleto">Boleto</label>
-            </div>
-          </div>
+          <CheckoutInputs />
           <button
             type="submit"
             className="bg-blue-500
@@ -289,9 +86,7 @@ class Checkout extends Component {
             py-2 px-4 border border-blue-700 rounded mt-5 mb-5"
           >
             Comprar
-
           </button>
-
         </section>
         {redirect && <Redirect to="/" Component={ Home } />}
       </section>
---
+++ src/Components/CheckoutInputs.jsx
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+
+export class CheckoutInputs extends Component {
+  render() {
+    const inputs = ['Nome completo', 'CPF',
+      'Endereço', 'Email', 'Cidade', 'Telefone', 'CEP'];
+
+    return (
+      <>
+        <form className="form-checkout">
+          <div className="flex md:w-1/2 flex-wrap -mx-3 mb-6 mt-10">
+            {inputs.map((type, index) => (
+              <>
+                <div key={ index } className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                  <label
+                    className="block uppercase
+                 tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    htmlFor={ index }
+                  >
+                    {type}
+                  </label>
+                  <input
+                    className="appearance-none
+                  block w-full bg-gray-200 text-gray-700 border
+                  rounded py-3 px-4 mb-3 leading-tight
+                  focus:outline-none focus:bg-white focus:border-gray-500"
+                    id={ index }
+                    type="text"
+                    placeholder={ type }
+                  />
+                </div>
+                {inputs.length - 1 === index && (
+                  <div className="w-full relative md:w-1/2 px-1 mb-6 md:mb-0 mt-6">
+                    <select
+                      className="block
+                   w-full bg-gray-200 border
+                    border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded
+                     leading-tight focus:outline-none
+                     focus:bg-white focus:border-gray-500"
+                      id="grid-state"
+                    >
+                      <option>SP</option>
+                      <option>RJ</option>
+                      <option>MG</option>
+                    </select>
+                  </div>
+                )}
+              </>
+            ))}
+          </div>
+        </form>
+        <div className="flex-col flex-wrap">
+          <h4
+            className="mt-10 font-sans text-lg
+             text-gray-800 text-center font-bold mb-5"
+          >
+            {' '}
+            Forma de pagamento
+          </h4>
+        </div>
+        <div className="flex-col mb-5 mt-10 payment-check space-x-2">
+          <div className="flex payment-check space-x-2">
+            <img alt="card" src="https://img.icons8.com/ios/50/000000/bank-card-back-side.png" />
+            {['Visa', 'Mastercard', 'Elo'].map((method, index) => (
+              <label key={ index } htmlFor={ method }>
+                <input type="radio" name="payment" id={ method } />
+                {' '}
+                {method}
+              </label>
+            ))}
+          </div>
+          <div className="flex payment-check space-x-2">
+            <img alt="bank slip" src="https://img.icons8.com/ios/50/000000/barcode.png" />
+            <label htmlFor="Boleto">
+              <input type="radio" name="payment" id="Boleto" />
+              {' '}
+              Boleto
+            </label>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default CheckoutInputs;
