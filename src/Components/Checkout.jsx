5a34744a39b52b862b393dfd260435d136247482
Exodin1 <exodin1@outlook.com>
2021-12-22 20:41:55 -0300

    Adicionando imagens e arrumando card dos produtos

--- src/Components/Checkout.jsx
+++ src/Components/Checkout.jsx
@@ -82,7 +82,11 @@ class Checkout extends Component {
               <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                 <label
                   className="block uppercase
-                 tracking-wide text-gray-700 text-xs font-bold mb-2"
+                 tracking-wide
+                  text-gray-700
+                   text-xs
+                    font-bold
+                     mb-2"
                   htmlFor="grid-first-name"
                 >
                   Nome completo
